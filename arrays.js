755c7ad35de6ac1fcaa62378021b89266023f811
priihbattie <135707297+priihbattie@users.noreply.github.com>
2024-04-01 10:51:05 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,11 @@
+const numero = [90, 17, 9, 256,1];
+
+console.log(numero);
+
+const cidades = ['Foz do Iguaçu', 'Cascavel', 'Londrina', 'Ponta Grossa', 'Ceu Azul', 'Cerra Dourada', 'Santa Terezinha', 'Curitiba', 'Matelandia', 'Umuarama'];
+
+console.log(cidades);
+
+const frutas = ['Banana', 'Acerola', 'Goiaba', 'Amora', 'Jaca', 'Pitaia', 'Uva', 'Morango'];
+
+console.log(frutas);
